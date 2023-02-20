5825c2cacc3ab4da597e0b0dfadb0c02d9f6da7d
RipanRenaldi25 <ripanrenaldi@student.telkomuniversity.ac.id>
2023-02-20 10:35:53 +0700

    Create Rabbit MQ pipeline Perubahan dan Penambahan yang telah dilakukan   * Membuat service product search sebagai consumer

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import SearchRouter from './src/routes/SearchRouter.js';
 import cors from 'cors';
+import amqplib from 'amqplib';
+import Products from './src/model/Products.js';
 dotenv.config();
 
 const app = express();
@@ -9,6 +11,34 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json('application/json'));
 app.use(cors(process.env.CORS_URL1));
 app.use(SearchRouter);
+
+const consume = async () => {
+    // make sure all exchange, queue is Exist
+    const connection = await amqplib.connect('amqp://localhost');
+    const channel = await connection.createChannel();
+    const que = await channel.assertQueue('getProducts');
+    //
+    console.log('Wait For Messages');
+    channel.consume(que.queue, async msg => {
+        const message = JSON.parse(msg.content.toString());
+        const {product, logType} = message;
+
+        console.log(product);
+        console.log(logType);
+        if( logType && (logType === 'create') ){
+            await Products.create(product);
+        }else if(logType && (logType === 'update')){
+            await Products.update(product, {where: {
+                id: product.id
+            }});
+        }else if( logType && (logType === 'delete') ){
+            await Products.destroy({where: {id: product.id}});
+        }
+        channel.ack(msg);
+    })
+    
+}
+consume();
 
 app.listen(parseInt(process.env.PORT), ()=>{
     console.log(`Server Listen on PORT ${process.env.PORT}`);
--- src/routes/SearchRouter.js
+++ src/routes/SearchRouter.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getAllProducts, getProductById, getProductsByCategory, getProductsByTitle } from '../controller/ProductController.js';
+import { getAllProducts, getProductById, getProductCategories, getProductsByCategory, getProductsByTitle } from '../controller/ProductController.js';
 import cache from '../middleware/cache.js';
 const SearchRouter = Router();
 
 SearchRouter.get('/products', cache(10000), getAllProducts);
 SearchRouter.get('/product/:id', getProductById);
 SearchRouter.get('/products/search', getProductsByTitle);
-SearchRouter.get("/products/categories/:category", getProductsByCategory);
+SearchRouter.get("/products/categories", getProductCategories);
+SearchRouter.get("/products/:category", getProductsByCategory);
 
 export default SearchRouter;
